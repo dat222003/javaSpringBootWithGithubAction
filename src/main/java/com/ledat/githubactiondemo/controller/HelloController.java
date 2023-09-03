a9abf5fa26a06f8838c34d8de1507ae300c351b1
dat222003 <dat.lt.1892@aptechlearning.edu.vn>
2023-09-03 16:53:27 +0700

    edit yaml workflow

--- src/main/java/com/ledat/githubactiondemo/controller/HelloController.java
+++ src/main/java/com/ledat/githubactiondemo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "edited yml file to stop workflow after run spring boot app";
+        return "hello world";
     }
 }
